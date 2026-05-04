fd6d2ae5b41352c7042f9d6afc3e3129bb067d2e
Jerome Oriol <jeromeoriol1964@gmail.com>
2026-05-04 12:29:13 +0200

    Fix homepage redirect to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
+import { redirect } from 'next/navigation'
+
 export default function Home() {
-  return (
-    <div style={{
-      minHeight: '100vh',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      fontFamily: 'sans-serif'
-    }}>
-      <h1>ETHYS Platform</h1>
-    </div>
-  )
+  redirect('/login')
 }
