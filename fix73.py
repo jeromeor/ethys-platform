cab0696ad105df3b4a0b4ee01ee9ff803261edb8
Jerome Oriol <jeromeoriol1964@gmail.com>
2026-05-07 17:52:48 +0200

    Fix Admin tabs double closing bracket

---
+++ fix73.py
@@ -0,0 +1,9 @@
+﻿with open('src/components/modules/AdminClient.tsx', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+# Ligne 211 contient maintenant ")}" en double - supprimer le premier
+lines[210] = "        )}\n"  # fermeture du map TABS - enlever le premier ))}
+
+with open('src/components/modules/AdminClient.tsx', 'w', encoding='utf-8') as f:
+    f.writelines(lines)
+print('Done')
---
+++ fix74.py
@@ -0,0 +1,9 @@
+﻿with open('src/components/modules/AdminClient.tsx', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+
+lines[210] = "          }}>{t}</button>\n"
+lines[211] = "        ))}\n"
+
+with open('src/components/modules/AdminClient.tsx', 'w', encoding='utf-8') as f:
+    f.writelines(lines)
+print('Done')
