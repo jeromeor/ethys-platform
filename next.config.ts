78a6670a755f627bbebd819cd09bfd91e4fa0d68
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-06-04 06:56:27 +0200

    Integrate next-intl plugin for translations

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+// Branche next-intl sur le fichier de config des traductions
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
