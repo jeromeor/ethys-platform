11603b6ecbf4ff500e3f4b13f63f917338538171
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:46:15 +0200

    Suppression scripts fix*.py obsoletes

--- fix195.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/components/layout/SidebarLayout.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[83] = "                <span style={{ flexShrink: 0, width: 20, display: 'flex', alignItems: 'center', justifyContent: 'center' }} dangerouslySetInnerHTML={{ __html: item.icon }} />\n"
-
-with open('src/components/layout/SidebarLayout.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print("Done")
