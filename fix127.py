348ecdde85df9ca768b25cffd2cce88d2a48b31c
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:45:06 +0200

    Suppression scripts fix*.py obsoletes

--- fix127.py
+++
@@ -1,8 +0,0 @@
-﻿with open("src/components/modules/CommandesClient.tsx", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-lines[399] = "                  {[['mixte', 'Fil ETHYS (recycl\u00e9 + vierge)']].map(([v, l]) => (\n"
-
-with open("src/components/modules/CommandesClient.tsx", "w", encoding="utf-8") as f:
-    f.writelines(lines)
-print("Done")
