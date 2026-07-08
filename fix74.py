b47be1da78754f600dc1846fbd8b7219de85fcfc
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:48:39 +0200

    Suppression scripts fix*.py obsoletes

--- fix74.py
+++
@@ -1,9 +0,0 @@
-﻿with open('src/components/modules/AdminClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[210] = "          }}>{t}</button>\n"
-lines[211] = "        ))}\n"
-
-with open('src/components/modules/AdminClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
