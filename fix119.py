30e2cf20b207473aa946932939276a8d2f5e9b26
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:44:58 +0200

    Suppression scripts fix*.py obsoletes

--- fix119.py
+++
@@ -1,4 +0,0 @@
-﻿with open("src/components/modules/MessagerieClient.tsx", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-for i in range(91, 102):
-    print(str(i+1) + ": " + lines[i].rstrip())
