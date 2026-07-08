bd640c6563d0ffc6ad3499a5984fa54e4fc4abf4
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:48:34 +0200

    Suppression scripts fix*.py obsoletes

--- fix7.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/components/modules/ProductionClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[173] = "                  }}>{cmd.priorite}</span>\n"
-
-with open('src/components/modules/ProductionClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
