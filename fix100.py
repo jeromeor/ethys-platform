e98e23075ae51dac88d2f62a10068e3c6dc9aed8
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:44:41 +0200

    Suppression scripts fix*.py obsoletes

--- fix100.py
+++
@@ -1,6 +0,0 @@
-﻿with open('src/components/modules/ProductionClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-# Trouver la fonction qui contient l update
-for i in range(68, 92):
-    print(str(i+1) + ': ' + lines[i].rstrip())
