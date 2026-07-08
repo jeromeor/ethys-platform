bf8e891ea1c8ae054ea3780bf451a4a8ae56e407
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:48:07 +0200

    Suppression scripts fix*.py obsoletes

--- fix41.py
+++
@@ -1,6 +0,0 @@
-﻿with open('src/components/modules/QRCodeClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-for i, line in enumerate(lines):
-    if 'GénérerQR' in line or 'G\u00e9n\u00e9rerQR' in line:
-        print(str(i+1) + ': ' + line.strip())
