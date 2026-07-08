81318d4d2d9032d866afca91f0a8ff661cbd094d
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:48:40 +0200

    Suppression scripts fix*.py obsoletes

--- fix75.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/components/modules/AdminClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-del lines[210]  # supprimer la ligne dupliquee }}>{t}</button>
-
-with open('src/components/modules/AdminClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
