ec09929076a0e9f3b035cb80880368cadb42ab0d
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:48:04 +0200

    Suppression scripts fix*.py obsoletes

--- fix39.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/components/modules/QRCodeClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[173] = "          {lots.length === 0 ? (\n"
-
-with open('src/components/modules/QRCodeClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
