7b76d8ba906b6f69f1adada4e4c1cc0c222e19a0
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:44:40 +0200

    Suppression scripts fix*.py obsoletes

--- fix10.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/components/modules/CommandesClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[40] = "  soumise:                'Soumise',\n"
-
-with open('src/components/modules/CommandesClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
