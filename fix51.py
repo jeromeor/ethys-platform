0386f0f20d8eef0ebd4c37751bae128371f18363
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:48:17 +0200

    Suppression scripts fix*.py obsoletes

--- fix51.py
+++
@@ -1,4 +0,0 @@
-﻿content = open('fix51_content.txt', 'r', encoding='utf-8').read()
-with open('src/app/tracabilite/[qrId]/page.tsx', 'w', encoding='utf-8') as f:
-    f.write(content)
-print('Done')
