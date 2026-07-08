fa2c18311231df95330744f8b469162cc8d658b5
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:47:28 +0200

    Suppression scripts fix*.py obsoletes

--- fix271.py
+++
@@ -1,9 +0,0 @@
-﻿with open('src/app/(dashboard)/profil/page.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[186] = "    </div>\n  )\n}\n"
-del lines[187]
-
-with open('src/app/(dashboard)/profil/page.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print("Done")
