b9cc9e3b00b51d2123bc60a637c7b9b9d0ef55a4
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:49:01 +0200

    Suppression scripts fix*.py obsoletes

--- fix95.py
+++
@@ -1,5 +0,0 @@
-﻿with open('src/components/modules/CommandesClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines):
-    if '<td' in line and i > 220 and i < 290:
-        print(str(i+1) + ': ' + line.strip())
