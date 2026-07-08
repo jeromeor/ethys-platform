a9b41e875a33f9413b1c405c77c79ee536874cf1
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:46:02 +0200

    Suppression scripts fix*.py obsoletes

--- fix182.py
+++
@@ -1,6 +0,0 @@
-﻿with open("src/app/tracabilite/[qrId]/page.tsx", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-# Verifier les lignes 40-46
-for i in range(38, 48):
-    print(str(i+1) + ': ' + repr(lines[i].rstrip()))
