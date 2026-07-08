730bea6fc42a42342181b6139d129eefa4858a5d
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:45:52 +0200

    Suppression scripts fix*.py obsoletes

--- fix172.py
+++
@@ -1,8 +0,0 @@
-﻿with open("src/app/tracabilite/[qrId]/page.tsx", "r", encoding="utf-8") as f:
-    lines = f.readlines()
-
-del lines[43]  # supprimer ligne 44
-
-with open("src/app/tracabilite/[qrId]/page.tsx", "w", encoding="utf-8") as f:
-    f.writelines(lines)
-print("Done")
