815be0de13fd3c988975731baebd249d7112b92f
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:47:47 +0200

    Suppression scripts fix*.py obsoletes

--- fix29.py
+++
@@ -1,9 +0,0 @@
-﻿with open('src/components/modules/QRCodeClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[146] = "              ['R\u00e9f\u00e9rence lot', selected.reference],\n"
-lines[62] = "    const urlPublique = `${window.location.origin}/tracabilite/${reference}`\n"
-
-with open('src/components/modules/QRCodeClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
