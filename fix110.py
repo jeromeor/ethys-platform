43e5d04f0ff076961145f35a0d28884100ca9321
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:44:50 +0200

    Suppression scripts fix*.py obsoletes

--- fix110.py
+++
@@ -1,9 +0,0 @@
-﻿with open('src/components/modules/ProductionClient.tsx', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace("'controle qualite'", "'contr\u00f4le qualit\u00e9'")
-content = content.replace('"controle qualite"', '"contr\u00f4le qualit\u00e9"')
-
-with open('src/components/modules/ProductionClient.tsx', 'w', encoding='utf-8') as f:
-    f.write(content)
-print('Done')
