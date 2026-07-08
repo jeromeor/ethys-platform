67c75b9f65d08522a543313443d0ad5f2e65c623
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:44:48 +0200

    Suppression scripts fix*.py obsoletes

--- fix108.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/components/modules/ProductionClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[124] = "        volume_tonnes: parseFloat(newLot.volume_tonnes) / 1000,\n"
-
-with open('src/components/modules/ProductionClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
