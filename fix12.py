2fa9eee375f926f04f1193fbf84ee2cf38a5a5b7
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:44:59 +0200

    Suppression scripts fix*.py obsoletes

--- fix12.py
+++
@@ -1,9 +0,0 @@
-﻿with open('src/components/modules/CommandesClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[40] = "  soumise:                'Soumises',\n"
-lines[48] = "  livree:                 'Livr\u00e9es',\n"
-
-with open('src/components/modules/CommandesClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
