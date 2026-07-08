e41d56e3644ef429e92c252a17d4a53689f37213
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:48:42 +0200

    Suppression scripts fix*.py obsoletes

--- fix77.py
+++
@@ -1,9 +0,0 @@
-﻿with open('src/components/layout/SidebarLayout.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-# Fermer le div avant </header>
-lines[120] = "          </div>\n        </header>\n"
-
-with open('src/components/layout/SidebarLayout.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
