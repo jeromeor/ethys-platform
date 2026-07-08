8479232d40a8694dd7104e151e592666df2c69d5
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:47:59 +0200

    Suppression scripts fix*.py obsoletes

--- fix34.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/components/modules/CertificationClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[165] = "      setForm({ type_produit: 'fil', volume_recycle_kg: '', volume_vierge_kg: '', provenance_pays: '', filature_nom: '', filature_pays: '', tisseur_nom: '', description: '', declaration_honneur: false })\n"
-
-with open('src/components/modules/CertificationClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print('Done')
