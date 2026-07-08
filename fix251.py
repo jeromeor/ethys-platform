aea838176ada905fb26928036dd652691819cf44
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:47:08 +0200

    Suppression scripts fix*.py obsoletes

--- fix251.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/components/modules/ProductionClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-del lines[259]  # supprimer ligne 260 (doublon vide)
-
-with open('src/components/modules/ProductionClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print("Done")
