f5d5a3b4ebff96adeef2fbb1c9a10c9f3d86f485
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:47:11 +0200

    Suppression scripts fix*.py obsoletes

--- fix255.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/components/modules/QRCodeClient.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-lines[350] = "                <div style={{ fontSize: 14, fontWeight: 900, color: '#c2956e', marginBottom: 4 }}>Fil certifi\u00e9 ETHYS</div>\n"
-
-with open('src/components/modules/QRCodeClient.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print("Done")
