bc6a9da04a83df0ae5c4baa63955eb3c1c93d2af
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:46:53 +0200

    Suppression scripts fix*.py obsoletes

--- fix236.py
+++
@@ -1,8 +0,0 @@
-﻿with open('src/app/tracabilite/[qrId]/page.tsx', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-del lines[149]  # supprimer ligne 150 - doublon titre
-
-with open('src/app/tracabilite/[qrId]/page.tsx', 'w', encoding='utf-8') as f:
-    f.writelines(lines)
-print("Done")
