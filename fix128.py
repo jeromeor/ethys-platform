d1290407263cfb8267bb071d091af39837d7a731
jeromeor <138809209+jeromeor@users.noreply.github.com>
2026-07-08 09:45:07 +0200

    Suppression scripts fix*.py obsoletes

--- fix128.py
+++
@@ -1,8 +0,0 @@
-﻿with open("src/components/modules/CommandesClient.tsx", "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = content.replace("type_coton: 'mixte'", "type_coton: 'mixte'")
-
-with open("src/components/modules/CommandesClient.tsx", "w", encoding="utf-8") as f:
-    f.write(content)
-print("Done")
